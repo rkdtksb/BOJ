0ba92204054ffa47532293c33759d07417d6e0d6
강산 <rkdtksb@gmail.com>
2023-01-30 15:25:13 +0900

    괄호

---
+++ 9012/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    static int T;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int cnt = 0;
+            String str = sc.next();
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') cnt++;
+                if (str.charAt(j) == ')') cnt--;
+                if (cnt < 0) break;
+            }
+            System.out.println(cnt == 0 ? "YES" : "NO");
+        }
+    }
+}
