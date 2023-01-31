559aa397fe5737ec773b0d90aafdc82b636f28e6
강산 <rkdtksb@gmail.com>
2023-01-31 13:31:47 +0900

    걷기

--- 1459/Main.java
+++ 1459/Main.java
@@ -20,12 +20,12 @@ public class Main {
         }
 
         if (W * 2 > S && W > S) {
-            ans = S * Math.max(X, Y);
-            if (Math.abs(X - Y) % 2 != 0) {
-                ans += W - S;
+            if (Math.abs(X - Y) % 2 == 0) {
+                ans = S * Math.max(X, Y);
+            } else { // Math.abs(X - Y) % 2 != 0
+                ans = S * (Math.max(X, Y) - 1) + W;
             }
         }
-        
         System.out.println(ans);
     }
 }
