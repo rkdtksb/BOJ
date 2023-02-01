fafded65157ef13be0089ad7d8f32232c69bb73d
강산 <rkdtksb@gmail.com>
2023-02-01 11:26:27 +0900

    괄호

--- 9012/Main.java
+++ 9012/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static int T;
-    static
+    static final String VALID = "YES", INVALID = "NO";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class Main {
                 if (ch == ')') cnt--;
                 if (cnt < 0) break;
             }
-            System.out.println(cnt == 0 ? "YES" : "NO");
+            System.out.println(cnt == 0 ? VALID : INVALID);
         }
     }
 }
