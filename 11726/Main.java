73425705372040895ccf2ef287f8f5106e519999
강산 <rkdtksb@gmail.com>
2023-02-02 17:00:39 +0900

    2xn 타일링

--- 11726/Main.java
+++ 11726/Main.java
@@ -8,10 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-
         d[1] = 1;
         d[2] = 2;
-
         for (int i = 3; i <= n; i++) {
             d[i] = (d[i - 2] + d[i - 1]) % MOD;
         }
