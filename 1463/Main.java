fe8bb388af706bd4a36236c622f2cde4c1879d69
강산 <rkdtksb@gmail.com>
2023-02-03 12:00:48 +0900

    1로 만들기

--- 1463/Main.java
+++ 1463/Main.java
@@ -11,8 +11,8 @@ public class Main {
         d[3] = 1;
         for (int i = 4; i <= N; i++) {
             d[i] = d[i - 1] + 1;
-            if (i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
             if (i % 2 == 0) d[i] = Math.min(d[i], d[i / 2] + 1);
+            if (i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
         }
         System.out.println(d[N]);
     }
