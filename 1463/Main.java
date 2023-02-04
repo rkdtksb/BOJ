38e046e57a33d7148ce24d6c17f328b4855a208f
강산 <rkdtksb@gmail.com>
2023-02-04 11:42:04 +0900

    1로 만들기

--- 1463/Main.java
+++ 1463/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
+        d[1] = 0;
         d[2] = 1;
-        d[3] = 1;
-        for (int i = 4; i <= N; i++) {
+        for (int i = 3; i <= N; i++) {
             d[i] = d[i - 1] + 1;
             if (i % 2 == 0) d[i] = Math.min(d[i], d[i / 2] + 1);
             if (i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
