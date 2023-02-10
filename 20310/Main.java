e74362a2d4c2e7e6a9a786d53838bedf0a21a85f
강산 <rkdtksb@gmail.com>
2023-02-10 19:16:32 +0900

    타노스

---
+++ 20310/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static String str;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '0') n++;
+            if (ch == '1') m++;
+        }
+        n /= 2;
+        m /= 2;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '0' && n > 0) {
+                System.out.print(0);
+                n--;
+            }
+            if (ch == '1' && m > 0) {
+                m--;
+            }
+            else if (ch == '1' && m == 0) {
+                System.out.print(1);
+            }
+        }
+    }
+}
