c6de487f07ed145bdb425961dfe98cfee52f9089
강산 <rkdtksb@gmail.com>
2023-02-11 19:31:43 +0900

    A → B

--- 16953/Main.java
+++ 16953/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     static int A, B;
     static int ans;
-    static final int IMMUTABLE = -1;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,19 +14,16 @@ public class Main {
                 B /= 2;
                 ans++;
             }
+            else if (B % 10 == 1) {
+                B /= 10;
+                ans++;
+            }
             else {
-                if (B % 10 == 1) {
-                    B -= 1;
-                    B /= 10;
-                    ans++;
-                }
-                else {
-                    ans = -1;
-                    break;
-                }
+                ans = -1;
+                break;
             }
         }
-        if (B < A) System.out.println(IMMUTABLE);
+        if (B != A) System.out.println(-1);
         else System.out.println(ans);
     }
 }
