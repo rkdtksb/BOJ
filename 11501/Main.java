a4548ef0e55de10b571a71661648ec71eca2f645
강산 <rkdtksb@gmail.com>
2023-02-14 16:40:42 +0900

    주식

---
+++ 11501/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    static int T;
+    static long[] a = new long[1000005];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            long ans = 0;
+            long cur = 0;
+            for (int j = 1; j <= N; j++) {
+                a[j] = sc.nextInt();
+            }
+            cur = a[N];
+            for (int j = N - 1; j > 0; j--) {
+                cur = Math.max(cur, a[j]);
+                ans += cur - a[j];
+            }
+            System.out.println(ans);
+        }
+    }
+}
