18686e3194b42da41ec1f56807660914e2a47964
강산 <rkdtksb@gmail.com>
2023-02-17 19:24:14 +0900

    RGB거리

--- 1149/Main.java
+++ 1149/Main.java
@@ -4,6 +4,11 @@ public class Main {
     static int N;
     static int[][] d = new int[1005][3];
 
+    static int min(int a, int b) {
+        if (a < b) return a;
+        else return b;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -13,10 +18,10 @@ public class Main {
             d[i][2] = sc.nextInt();
         }
         for (int i = 1; i <= N; i++) {
-            d[i][0] += Math.min(d[i - 1][1], d[i - 1][2]);
-            d[i][1] += Math.min(d[i - 1][0], d[i - 1][2]);
-            d[i][2] += Math.min(d[i - 1][0], d[i - 1][1]);
+            d[i][0] += min(d[i - 1][1], d[i - 1][2]);
+            d[i][1] += min(d[i - 1][0], d[i - 1][2]);
+            d[i][2] += min(d[i - 1][0], d[i - 1][1]);
         }
-        System.out.println(Math.min(d[N][0], Math.min(d[N][1], d[N][2])));
+        System.out.println(min(d[N][0], min(d[N][1], d[N][2])));
     }
 }
