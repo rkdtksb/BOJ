650b194257531de4cd72a4051bc15aa91960d78b
강산 <rkdtksb@gmail.com>
2023-02-18 18:21:15 +0900

    배열 복원하기

--- 16967/Main.java
+++ 16967/Main.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    static int H, W, X, Y;
-    static int[][] B = new int[605][605];
+    static int h, w, x, y;
+    static int[][] b = new int[605][605];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        H = sc.nextInt();
-        W = sc.nextInt();
-        X = sc.nextInt();
-        Y = sc.nextInt();
-        for (int i = 0; i < H + X; i++) {
-            for (int j = 0; j < W + Y; j++) {
-                B[i][j] = sc.nextInt();
+        h = sc.nextInt();
+        w = sc.nextInt();
+        x = sc.nextInt();
+        y = sc.nextInt();
+        for (int i = 0; i < h + x; i++) {
+            for (int j = 0; j < w + y; j++) {
+                b[i][j] = sc.nextInt();
             }
         }
-        for (int i = X; i < H; i++) {
-            for (int j = Y; j < W; j++) {
-                B[i][j] -= B[i - X][j - Y];
+        for (int i = x; i < h; i++) {
+            for (int j = y; j < w; j++) {
+                b[i][j] -= b[i - x][j - y];
             }
         }
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                System.out.print(B[i][j] + " ");
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.print(b[i][j] + " ");
             }
             System.out.println();
         }
