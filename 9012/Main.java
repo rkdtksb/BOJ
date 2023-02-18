c620767c22ed5630857da900b4fc73c7315be174
강산 <rkdtksb@gmail.com>
2023-02-18 18:26:56 +0900

    괄호

--- 9012/Main.java
+++ 9012/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    static int T;
+    static int t;
     static final String VALID = "YES", INVALID = "NO";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
+        t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
             int cnt = 0;
             String str = sc.next();
             for (int j = 0; j < str.length(); j++) {
