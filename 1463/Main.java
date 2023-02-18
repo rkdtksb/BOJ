1ab63f98ec0e16a27ace726bbb1a80f7ce30babe
강산 <rkdtksb@gmail.com>
2023-02-18 18:19:40 +0900

    1로 만들기

--- 1463/Main.java
+++ 1463/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N;
+    static int n;
     static int[] d = new int[1000005];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        n = sc.nextInt();
         d[1] = 0;
         d[2] = 1;
-        for (int i = 3; i <= N; i++) {
+        for (int i = 3; i <= n; i++) {
             d[i] = d[i - 1] + 1;
             if (i % 2 == 0) d[i] = Math.min(d[i], d[i / 2] + 1);
             if (i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
         }
-        System.out.println(d[N]);
+        System.out.println(d[n]);
     }
 }
