9ed4e05140726f5eb324e811c2f6f74384026d06
강산 <rkdtksb@gmail.com>
2023-02-18 18:18:51 +0900

    주유소

--- 13305/Main.java
+++ 13305/Main.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N;
+    static int n;
     static long ans, dis;
     static long[] a = new long[100005];
     static long[] b = new long[100005];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        for (int i = 0; i < N - 1; i++) {
+        n = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
             a[i] = sc.nextLong();
         }
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             b[i] = sc.nextLong();
         }
         ans = a[0] * b[0];
         dis = b[0];
-        for (int i = 1; i < N - 1; i++) {
+        for (int i = 1; i < n - 1; i++) {
             dis = Math.min(dis, b[i]);
             ans += a[i] * dis;
         }
