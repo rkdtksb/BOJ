0b1bd2ba55ea7b65f6768ea5178e81e0a7e8200f
강산 <rkdtksb@gmail.com>
2023-02-19 11:33:40 +0900

    괄호

--- 9012/Main.java
+++ 9012/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
             int cnt = 0;
             String str = sc.next();
-            for (int j = 0; j < str.length(); j++) {
-                char ch = str.charAt(j);
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
                 if (ch == '(') cnt++;
                 if (ch == ')') cnt--;
                 if (cnt < 0) break;
