413e4433b8fd60be339ebea69435d9c5a458e1d5
강산 <rkdtksb@gmail.com>
2023-02-25 09:37:37 +0900

    바이러스

--- 2606/Main.java
+++ 2606/Main.java
@@ -1,61 +1,36 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static class Pair {
-        int x, y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static int n, m, k;
+    static int n, m;
     static int ans;
-    static int[][] a = new int[105][105];
-    static Queue<Pair> Q = new ArrayDeque<>();
+    static boolean[] vis = new boolean[105];
+    static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    static int bfs(int x, int y) {
-        int cnt = 1;
-        a[x][y] = 0;
-        Q.offer(new Pair(x, y));
-        while (!Q.isEmpty()) {
-            Pair cur = Q.peek(); Q.poll();
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if (nx <= 0 || ny <= 0 || nx > n || ny > m) continue;
-                if (a[nx][ny] == 0) continue;
-                cnt++;
-                a[nx][ny] = 0;
-                Q.offer(new Pair(nx, ny));
+    static void dfs(int cur) {
+        vis[cur] = true;
+        for (int nxt : adj.get(cur)) {
+            if (!vis[nxt]) {
+                ans++;
+                dfs(nxt);
             }
         }
-        return cnt;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        k = sc.nextInt();
-        for (int i = 0; i < k; i++) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-            a[r][c] = 1;
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a[i][j] == 1) {
-                    ans = Math.max(ans, bfs(i, j));
-                }
-            }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
+        dfs(1);
         System.out.println(ans);
     }
 }
