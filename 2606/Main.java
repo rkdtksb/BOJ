415f4b49d710a721a9910ade26b9addb7c782b27
강산 <rkdtksb@gmail.com>
2023-02-25 09:31:58 +0900

    바이러스

--- 2606/Main.java
+++ 2606/Main.java
@@ -21,10 +21,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        while (n-- >= 0) {
+        for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
-        while (m-- > 0) {
+        for (int i = 0; i < m; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             adj.get(u).add(v);
