83f419d3a2b9bff7fcf22b092450eee17d592bc2
강산 <rkdtksb@gmail.com>
2023-03-04 11:08:52 +0900

    회전 초밥

--- 2531/Main.java
+++ 2531/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static int n, d, k, c;
-    static int ans, cnt;
+    static int ans;
     static int[] a = new int[30005];
     static int[] b = new int[3005];
 
@@ -15,17 +15,17 @@ public class Main {
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-        cnt = 1;
+        int cnt = 1;
         b[c]++;
         for (int i = 0; i < k; i++) {
             if (b[a[i]] == 0) cnt++;
             b[a[i]]++;
         }
         for (int i = k; i < k + n; i++) {
-            if (b[a[i % n]] == 0) cnt++;
-            b[a[i % n]]++;
             if (b[a[i - k]] == 1) cnt--;
             b[a[i - k]]--;
+            if (b[a[i % n]] == 0) cnt++;
+            b[a[i % n]]++;
             ans = Math.max(ans, cnt);
         }
         System.out.println(ans);
