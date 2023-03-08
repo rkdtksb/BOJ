618aa3af2c03aab73dac0fc7448c7fe48791f272
강산 <rkdtksb@gmail.com>
2023-03-08 11:04:59 +0900

    빗물

--- 14719/Main.java
+++ 14719/Main.java
@@ -9,24 +9,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         h = sc.nextInt();
         w = sc.nextInt();
-        int mxh = 0;
-        int mxi = 0;
         for (int i = 0; i < w; i++) {
             a[i] = sc.nextInt();
-            if (a[i] > mxh) {
-                mxh = a[i];
-                mxi = i;
+        }
+        for (int i = 0; i < w; i++) {
+            int l = 0;
+            int r = 0;
+            for (int j = i; j >= 0; j--) {
+                l = Math.max(l, a[j]);
             }
-        }
-        int st = a[0];
-        int en = a[w];
-        for (int i = 0; i < mxi; i++) {
-            st = Math.max(st, a[i]);
-            ans += st - a[i];
-        }
-        for (int i = w - 1; i > mxi; i--) {
-            en = Math.max(en, a[i]);
-            ans += en - a[i];
+            for (int j = i; j < w; j++) {
+                r = Math.max(r, a[j]);
+            }
+            ans += Math.min(l, r) - a[i];
         }
         System.out.println(ans);
     }
